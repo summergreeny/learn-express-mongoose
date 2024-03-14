a7f1b613440c01b60ae3b8d900d0663fc2b18600
yilinliu <yilin.liu@hotmail.com>
2024-03-14 13:38:16 -0400

    all activitties

--- models/genre.js
+++ models/genre.js
@@ -1,11 +1,10 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
-var GenreSchema = new Schema(
-  {}
-);
-
+var GenreSchema = new Schema({
+  name: { type: String, required: true, maxLength: 100, minLength: 4 },
+});
 
 //Export model
-module.exports = mongoose.model('Genre', GenreSchema);
+module.exports = mongoose.model("Genre", GenreSchema);
