78ecdffef7a3287cdc90ce67c8437fcc85b9f216
yilinliu <yilin.liu@hotmail.com>
2024-03-30 20:57:51 -0400

    activities

--- pages/books.js
+++ pages/books.js
@@ -1,20 +1,19 @@
-let Book = require('../models/book');
-let Author = require('../models/author');
+let Book = require("../models/book");
+let Author = require("../models/author");
 
-function get_books () {
-  return Book.find({}, 'title author')
-    .sort({title : 1})  // 1 indictes ascending order
-    .populate('author');
+function get_books() {
+  return Book.find({}, "title author")
+    .sort({ title: 1 }) // 1 indictes ascending order
+    .populate("author"); // author is ObjectID
 }
 
 exports.show_books = async () => {
   try {
     let books = await get_books().exec();
-    return books.map(function(b) {
-      return b._id + ' : ' + b.title + ' : ' + Author(b.author).name;
+    return books.map(function (b) {
+      return b._id + " : " + b.title + " : " + Author(b.author).name;
     });
+  } catch (err) {
+    console.log("Could not get books " + err);
   }
-  catch(err) {
-    console.log('Could not get books ' + err);
-  }
-}
+};
--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,15 @@
-let BookInstance = require('../models/bookinstance');
+let BookInstance = require("../models/bookinstance");
 
-exports.show_all_books_status = function(res) {
-  return res.send([]);
-}
+exports.show_all_books_status = function (res) {
+  BookInstance.find({ status: { $eq: "Available" } })
+    .populate("book")
+    .exec()
+    .then((list_bookinstances) => {
+      res.send(
+        list_bookinstances.map(function (bookInstance) {
+          return bookInstance.book.title + " : " + bookInstance.status;
+        })
+      );
+    })
+    .catch((err) => res.send("Status not found"));
+};
